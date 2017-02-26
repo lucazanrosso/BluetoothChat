91083b7edc70149759dd00bf0cd71902a9d1646b
lucazanrosso <lucazanrosso94@gmail.com>
2017-02-26 13:21:02 +0100

    Improve project

--- app/src/main/java/com/lucazanrosso/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/lucazanrosso/bluetoothchat/MainActivity.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -21,18 +22,18 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final String TAG = "SI";
+    public final String TAG = "Error";
     public final int MESSAGE_READ = 1;
+    public final int REQUEST_ENABLE_BT = 2;
 
-    Handler mHandler;
+    static Handler mHandler;
 
-    private BluetoothAdapter btAdapter = null;
+    private BluetoothAdapter mBluetoothAdapter = null;
     private BluetoothSocket btSocket = null;
 
     private ConnectedThread mConnectedThread;
 
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private static String address = "98:D3:32:20:68:87";
 
     LinearLayout conversationLayout;
 
@@ -42,15 +43,26 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        conversationLayout = (LinearLayout) findViewById(R.id.layout_conversation);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not found in this device", Toast.LENGTH_SHORT).show();
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                connect();
+            }
+        }
 
+        conversationLayout = (LinearLayout) findViewById(R.id.layout_conversation);
         mHandler = new Handler() {
             public void handleMessage(android.os.Message msg) {
                 switch (msg.what) {
                     case MESSAGE_READ:
                         byte[] readBuf = (byte[]) msg.obj;
                         String readString = new String(readBuf, 0, msg.arg1);
-                        View inflatedLayout = getLayoutInflater().inflate(R.layout.text_view_received, null, false);
+                        View inflatedLayout = getLayoutInflater().inflate(R.layout.text_view_received,conversationLayout, false);
                         TextView textView = (TextView) inflatedLayout.findViewById(R.id.textview_receive);
                         textView.setText(readString);
                         conversationLayout.addView(inflatedLayout);
@@ -58,9 +70,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
-        btAdapter = BluetoothAdapter.getDefaultAdapter();       // get Bluetooth adapter
-//        checkBTState();
     }
 
     public void sendMessage(View view) {
@@ -68,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         mConnectedThread.write(message);
         editText.setText(null);
-        View inflatedLayout = getLayoutInflater().inflate(R.layout.text_view_sent, null, false);
+        View inflatedLayout = getLayoutInflater().inflate(R.layout.text_view_sent, conversationLayout, false);
         TextView textView = (TextView) inflatedLayout.findViewById(R.id.textview_send);
         textView.setText(message);
         conversationLayout.addView(inflatedLayout);
@@ -80,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         private final OutputStream mmOutStream;
         private byte[] mmBuffer; // mmBuffer store for the stream
 
-        public ConnectedThread(BluetoothSocket socket) {
+        private ConnectedThread(BluetoothSocket socket) {
             mmSocket = socket;
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
@@ -124,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Call this from the main activity to send data to the remote device.
-        public void write(String message) {
+        private void write(String message) {
             try {
                 byte[] bytes = message.getBytes();
                 mmOutStream.write(bytes);
@@ -137,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Call this method from the main activity to shut down the connection.
-        public void cancel() {
+        private void cancel() {
             try {
                 mmSocket.close();
             } catch (IOException e) {
@@ -147,48 +156,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            connect();
+        }
+        if (resultCode == RESULT_CANCELED) {
+            finish();
+        }
+    }
 
-        Log.d(TAG, "...onResume - try connect...");
-
-        // Set up a pointer to the remote node using it's address.
-        BluetoothDevice device = btAdapter.getRemoteDevice(address);
-
-        // Two things are needed to make a connection:
-        //   A MAC address, which we got above.
-        //   A Service ID or UUID.  In this case we are using the
-        //     UUID for SPP.
-
+    public void connect() {
+        final String ADDRESS = "98:D3:32:20:68:87";
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(ADDRESS);
         try {
             btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
         } catch (IOException e) {
-            Toast.makeText(getBaseContext(), "Fatal Error" + " - " + "In onResume() and socket create failed: " + e.getMessage() + ".", Toast.LENGTH_LONG).show();
             finish();
         }
-
-        // Discovery is resource intensive.  Make sure it isn't going on
-        // when you attempt to connect and pass your message.
-        btAdapter.cancelDiscovery();
-
-        // Establish the connection.  This will block until it connects.
-        Log.d(TAG, "...Connecting...");
+        mBluetoothAdapter.cancelDiscovery();
         try {
             btSocket.connect();
-            Log.d(TAG, "....Connection ok...");
         } catch (IOException e) {
             try {
                 btSocket.close();
             } catch (IOException e2) {
-                Toast.makeText(getBaseContext(), "Fatal Error" + " - " + "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".", Toast.LENGTH_LONG).show();
                 finish();
             }
         }
-
-        // Create a data stream so we can talk to server.
-        Log.d(TAG, "...Create Socket...");
-
         mConnectedThread = new ConnectedThread(btSocket);
         mConnectedThread.start();
+        Toast.makeText(getApplicationContext(), "Connected with: " + ADDRESS, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mConnectedThread.cancel();
+        try {
+            btSocket.close();
+        } catch (IOException e2) {
+            finish();
+        }
+        super.onDestroy();
     }
 }
